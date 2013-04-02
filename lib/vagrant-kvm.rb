db09befdfedc0ccf4869df79460bf6d8ef77e830
Alex Drahon <adrahon@gmail.com>
2013-04-02 15:00:31 +0100

    Correct autoload issues

--- lib/vagrant-kvm.rb
+++ lib/vagrant-kvm.rb
@@ -6,9 +6,10 @@ module VagrantPlugins
   module ProviderKvm
     lib_path = Pathname.new(File.expand_path("../vagrant-kvm", __FILE__))
     autoload :Action, lib_path.join("action")
-    autoload :Driver, lib_path.join("driver")
+    autoload :Driver, lib_path.join("driver/driver")
     autoload :Errors, lib_path.join("errors")
-    autoload :Util, lib_path.join("util")
+    autoload :VmDefinition, lib_path.join("util/vm_definition")
+    autoload :NetworkDefinition, lib_path.join("util/network_definition")
 
     # This returns the path to the source of this plugin.
     #
