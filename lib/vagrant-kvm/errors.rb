bdf88bc81880e20484c4dc1bc8dc2ccd2b242153
Hiroshi Miura <miurahr@linux.com>
2013-08-04 14:33:37 +0900

    Fix reference error to Error
    -driver.rb: include Error
    -define KvmNoConnection vagrant error
    -Add error message for KvmNoConnection

--- lib/vagrant-kvm/errors.rb
+++ lib/vagrant-kvm/errors.rb
@@ -6,6 +6,9 @@ module VagrantPlugins
       class VagrantKVMError < Vagrant::Errors::VagrantError
         error_namespace("vagrant_kvm.errors")
       end
+      class KvmNoConnection < VagrantKVMError
+        error_key(:kvm_no_connection)
+      end
       class KvmInvalidVersion < VagrantKVMError
         error_key(:kvm_invalid_version)
       end
