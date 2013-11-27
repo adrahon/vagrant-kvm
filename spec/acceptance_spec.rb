00ee1e584cdda6accbae5c5c9fc60bec61602d14
Simon Mathieu <simon.math@gmail.com>
2013-11-27 14:08:07 -0800

    Some caveat added to the acceptance test

--- spec/acceptance_spec.rb
+++ spec/acceptance_spec.rb
@@ -2,16 +2,23 @@ require 'spec_helper'
 
 require Vagrant.source_root.join("plugins/kernel_v2/config/vm")
 
+# This test is far from perfect. If attemps to boot an empty machine, which
+# exercises a lot of this plugins code. It does not however makes any assertion
+# of the state of that machine. This needs improving in many ways, but is still
+# much better than nothing.
 
 describe "Vagrant KVM Plugin" do
   let(:folder) { "spec/tmp" }
   let(:environment) { Vagrant::Environment.new(cwd: folder) }
 
   before do
+    # Cleanup any previous import
     FileUtils.rm_rf "~/.vagrant.d/boxes/test_box/"
     FileUtils.mkdir_p folder
     FileUtils.cp test_file("Vagrantfile"), folder
 
+    # Vagrant will die trying to mounth these shares as we're not booting a
+    # *real* box. Stub that stuff out.
     VagrantPlugins::Kernel_V2::VMConfig.any_instance.stub(synced_folders: {})
     VagrantPlugins::ProviderKvm::Action::ShareFolders.any_instance.stub(
       prepare_folders: nil,
