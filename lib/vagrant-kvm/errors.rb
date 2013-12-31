01ea0fde9270f51469d9409a59cb8fdb7226474c
Simon Mathieu <simon.math@gmail.com>
2013-12-31 17:27:09 -0500

    Added missing translation for exceptions

--- lib/vagrant-kvm/errors.rb
+++ lib/vagrant-kvm/errors.rb
@@ -23,16 +23,12 @@ module VagrantPlugins
         error_key(:kvm_fail_image_conversion)
       end
 
-      class KvmImageUploadError < VagrantKVMError
-        error_key(:kvm_fail_image_conversion)
-      end
-
       class KvmBadBoxFormat < VagrantKVMError
-        error_key(:kvm_fail_image_conversion)
+        error_key(:kvm_bad_box_format)
       end
 
       class KvmFailedCommand < VagrantKVMError
-        error_key(:kvm_command_failed)
+        error_key(:kvm_failed_command)
       end
     end
   end
--- lib/vagrant-kvm/util/commands.rb
+++ lib/vagrant-kvm/util/commands.rb
@@ -4,7 +4,7 @@ module VagrantPlugins
       module Commands
         def run_command(cmd)
           unless res = system(cmd)
-            raise Errors::KvmFailedCommand, "System command #{cmd} returned with error code #{res}"
+            raise Errors::KvmFailedCommand, cmd: cmd, res: res
           end
           res
         end
