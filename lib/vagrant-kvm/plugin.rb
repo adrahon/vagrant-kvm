f08edb7a4fdaf17ac4dff7db8a1d0fb5db4dd8f2
Hiroshi Miura <miurahr@linux.com>
2014-01-02 23:08:01 +0900

    Now depend on vagrant 1.4.0 and later
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- lib/vagrant-kvm/plugin.rb
+++ lib/vagrant-kvm/plugin.rb
@@ -6,8 +6,8 @@ end
 
 # This is a sanity check to make sure no one is attempting to install
 # this into an early Vagrant version.
-if Vagrant::VERSION < "1.1.0"
-  raise "The Vagrant KVM plugin is only compatible with Vagrant 1.1+"
+if Vagrant::VERSION < "1.4.0"
+  raise "The Vagrant KVM plugin is only compatible with Vagrant 1.4+"
 end
 
 module VagrantPlugins
