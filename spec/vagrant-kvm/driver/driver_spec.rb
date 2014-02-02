4b39ee159049c8cd4832ffd604bbf2eabfd51199
Hiroshi Miura <miurahr@linux.com>
2014-02-02 23:31:39 +0900

    add spec test for driver
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- spec/vagrant-kvm/driver/driver_spec.rb
+++ spec/vagrant-kvm/driver/driver_spec.rb
@@ -6,6 +6,12 @@ module VagrantPlugins
       describe Driver do
         let(:xml) { test_file "box.xml" }
         let(:volume_name) { "" }
+        let(:disk_name) { 'spec-test' }
+        let(:box_path) { test_file "box-disk1.img" }
+        let(:box_pool) { test_file "" }
+        let(:capacity) { {:size=>256, :unit=>'KB'} }
+        let(:image_path) { '/tmp/pool-storage/box-disk1.img' }
+        let(:image_type) { 'qcow2' }
 
         before do
           described_class.any_instance.stub(:load_kvm_module!) { true }
@@ -23,6 +29,13 @@ module VagrantPlugins
             expect do
               subject.init_storage("/tmp")
             end.to_not raise_exception
+          end
+
+          it "does not raise exception" do
+            expect do
+              subject.create_volume(disk_name, capacity, image_path,
+                               image_type, box_pool, box_path, true)
+              end.to_not raise_exception
           end
 
           it "does not raise exception" do
