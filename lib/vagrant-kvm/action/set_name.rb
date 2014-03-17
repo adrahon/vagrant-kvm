839fd41baa261a99b748b8079e467ac62c96a880
Hiroshi Miura <miurahr@linux.com>
2014-03-17 09:29:50 +0900

    set name with machine name
    - use same way with virtualbox to set name
    - todo: vm name conflict check
       need driver func to detect existing vms
    
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- lib/vagrant-kvm/action/set_name.rb
+++ lib/vagrant-kvm/action/set_name.rb
@@ -10,17 +10,41 @@ module VagrantPlugins
         def call(env)
           name = env[:machine].provider_config.name
 
+          # If we already set the name before, then don't do anything
+          sentinel = env[:machine].data_dir.join("action_set_name")
+          if !name && sentinel.file?
+            @logger.info("Default name was already set before, not doing it again.")
+            return @app.call(env)
+          end
+
+          # If no name was manually set, then use a default
           if !name
-            prefix = env[:root_path].basename.to_s
+            prefix = "#{env[:root_path].basename.to_s}_#{env[:machine].name}"
             prefix.gsub!(/[^-a-z0-9_]/i, "")
-            name = prefix + "_#{Time.now.to_i}"
+            name = prefix + "_#{(Time.now.to_f * 1000.0).to_i}_#{rand(100000)}"
           end
 
           # @todo raise error if name is taken in kvm
-          # @todo don't set the name if the vm already has a name
+          # Verify the name is not taken
+          # vms = env[:machine].provider.driver.read_vms
+          #raise Vagrant::Errors::VMNameExists, :name => name if \
+          #  vms.has_key?(name) && vms[name] != env[:machine].id
+          #
+          #if vms.has_key?(name)
+          #  @logger.info("Not setting the name because our name is already set.")
+          #else
+          #  env[:ui].info(I18n.t(
+          #    "vagrant.actions.vm.set_name.setting_name", name: name))
+          #  env[:machine].provider.driver.set_name(name)
+          #end
 
           @logger.info("Setting the name of the VM: #{name}")
           env[:machine].provider.driver.set_name(name)
+
+          # Create the sentinel
+          sentinel.open("w") do |f|
+            f.write(Time.now.to_i.to_s)
+          end
 
           @app.call(env)
         end
